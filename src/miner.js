9a8ce772c4a2e49f85d6ae179d41c03e88a8d1a9
Shorent <mattshero40@gmail.com>
2023-08-05 21:01:31 -0500

    digger works now

--- src/miner.js
+++ src/miner.js
@@ -2,48 +2,58 @@ const vec3 = require('vec3')
 const goals = require('mineflayer-pathfinder').goals
 
 async function mineArea (bot, relativePosition) {
-    const area = new Area(bot.entity.position, bot.entity.position.plus(relativePosition))
+    const area = new Area(bot.entity.position,bot.entity.position.plus(relativePosition))
     let chunks = chunkArea(vec3(4,2,4),area);
-    console.log("Mining Area: ", area.startPos, area.endPos)
-    const originalPos = bot.entity.position
+    const originalBotPos = bot.entity.position
     for (const chunk of chunks){
-        await mineChunk(bot, originalPos, chunk);
+        await mineChunk(bot, originalBotPos, chunk);
     }
 }
 
 function mineChunk(bot, originalPos, relativeChunk){
     return new Promise( async (resolve, reject) => {
-    console.log("Mining chunk: ", relativeChunk)
-    let blockPositions = new Array()
-    for (let z = relativeChunk.startPos.z; z < relativeChunk.endPos.z; z++) 
-        for (let y = relativeChunk.startPos.y; y < relativeChunk.endPos.y; y++)
-            for (let x = relativeChunk.startPos.x; x < relativeChunk.endPos.x; x++)
-                blockPositions.push(vec3(x, y, z).plus(originalPos))
-    console.log(blockPositions.length)
-    for await (const blockPosition of blockPositions){
-        console.log("Mining ", blockPosition)
-        await mineBlockIfCapable(bot, blockPosition)
-    }
-    resolve()
+        let blockPositions = new Array()
+        const dirSignx = Math.sign(relativeChunk.endPos.x - relativeChunk.startPos.x)
+        const dirSigny = Math.sign(relativeChunk.endPos.y - relativeChunk.startPos.y)
+        const dirSignz = Math.sign(relativeChunk.endPos.z - relativeChunk.startPos.z)
+        for (let z = Math.abs(relativeChunk.startPos.z); z < Math.abs(relativeChunk.endPos.z); z++) 
+            for (let y = Math.abs(relativeChunk.startPos.y); y < Math.abs(relativeChunk.endPos.y); y++)
+                for (let x = Math.abs(relativeChunk.startPos.x); x < Math.abs(relativeChunk.endPos.x); x++)
+                    blockPositions.push(vec3(x*dirSignx, y*dirSigny, z*dirSignz).plus(originalPos))
+        console.log(blockPositions)
+        for await (const blockPosition of blockPositions){
+            await mineBlockIfCapable(bot, blockPosition)
+        }
+        resolve()
     })
 }
 
 function chunkArea(chunkSize, area){
-    console.log("generating Chunks...")
-    console.log(chunkSize, area)
     const chunksX = area.getXLen() / chunkSize.x
     const chunksY = area.getYLen() / chunkSize.y
     const chunksZ = area.getZLen() / chunkSize.z
-    console.log("chunk amt: ", chunksX, chunksY, chunksZ)
     const chunks = new Array();
+    const chunkXSizeSigned = chunkSize.x*Math.sign(area.relativePosition.x)
+    const chunkYSizeSigned = chunkSize.y*Math.sign(area.relativePosition.y)
+    const chunkZSizeSigned = chunkSize.z*Math.sign(area.relativePosition.z)
     //need to add remainder in the case that chunksX...etc is float
     for (let y = 0; y < chunksY; y++)
         for (let z = 0; z < chunksZ; z++)
             for (let x = 0; x < chunksX; x++)
-                chunks.push( new Area(vec3(x*chunkSize.x,y*chunkSize.y,z*chunkSize.z),
-                    vec3(x*chunkSize.x+chunkSize.x,y*chunkSize.y+chunkSize.y,z*chunkSize.z+chunkSize.z)))
-    console.log("generated chunks: ", chunks)
-    return chunks;
+                chunks.push( new Area(
+                    vec3(
+                        x*chunkXSizeSigned,
+                        y*chunkYSizeSigned,
+                        z*chunkZSizeSigned
+                    ),
+                    vec3(
+                        x*chunkXSizeSigned+chunkXSizeSigned,
+                        y*chunkYSizeSigned+chunkYSizeSigned,
+                        z*chunkZSizeSigned+chunkZSizeSigned
+                    )
+                ))
+    chunks.sort(sortByY);
+    return chunks
 }
 
 function mineBlockIfCapable(bot, blockPosition){
@@ -75,12 +85,17 @@ function mineBlockIfCapable(bot, blockPosition){
     });
 }
 
+function sortByY(a,b){
+    return b.startPos.y - a.startPos.y
+}
+
 class Area {
     constructor(startPos, endPos){
         this.startPos = startPos
         this.endPos = endPos
+        this.relativePosition = endPos.minus(startPos)
     }
-
+    
     getXLen(){
         return Math.abs(this.startPos.x - this.endPos.x)
     }
